2583f547def5dfebf70373cf25d083b965349ed5
Scorp3y <artem.turovetc@gmail.com>
2025-05-06 04:30:48 +0300

    Cleaned assets & added Telegram link (v1.0.3)

---
+++ Assets/Menu Files/OpenLink.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void OpenTelegram()
+    {
+        Application.OpenURL("https://t.me/+ruM-hYILfkk4NWU0");
+    }
+}
